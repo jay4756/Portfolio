bd7b1ae39decf1670487754de798fbbd275735b6
JAY PRAJAPATI <j3883089@gmail.com>
2026-05-14 08:44:20 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
          Java Full-Stack Developer | Web Developer | Data Scientist  
         </p>
         <a
-          href="/jay.pdf"
+          href="/jay_prajapati_cv.pdf"
           download
           className="inline-block bg-cyan-500 hover:bg-cyan-600 text-white font-semibold py-3 px-6 rounded-lg transition duration-300"
         >
